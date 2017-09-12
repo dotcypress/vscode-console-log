1c84ad4e8279e488c41bed4777a1ee86e07d42ca
Vitaly Domnikov <dotcypress@gmail.com>
2017-09-12 13:13:59 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# console-log
+
+[VS Code console.log generator](https://marketplace.visualstudio.com/items?itemName=dotcypress.console-log)
+
+Open command pallete and type: `> Generate console.log ` or just press `ctrl+shift+q`
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `console-log.semicolon`: enable/disable simicolon
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,38 @@
+var vscode = require('vscode');
+
+const insertText = (val) => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+    const selection = editor.selection;
+    const range = new vscode.Range(selection.start, selection.end);
+    editor.edit((editBuilder) => {
+        editBuilder.replace(range, val);
+    });
+}
+
+const normalizeSelection = (editor) => {
+    return editor.selection.isEmpty 
+        ? vscode.commands.executeCommand('editor.action.smartSelect.grow') 
+        : Promise.resolve();
+}
+
+exports.activate = function (context) {
+    const insertLogStatement = vscode.commands.registerCommand('extension.generateConsoleLog', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) { return; }
+        normalizeSelection(editor).then(() => {
+            const { semicolon } = vscode.workspace.getConfiguration('console-log');
+            const selection = editor.selection;
+            const text = editor.document.getText(selection).trim();            
+            vscode.commands.executeCommand('editor.action.insertLineAfter').then(() => text
+                ? insertText(`console.log('${text.replace(/'/g, '\\\'')}', ${text})${semicolon ? ';' : ''}`)
+                : insertText(`console.log()${semicolon ? ';' : ''}`)              
+            )                
+        });        
+    });
+    context.subscriptions.push(insertLogStatement);    
+};
+
+exports.deactivate = function () {};
